ddeb922ee457258cfbaf43719edc03bb13107e1c
kj-9 <46800811+kj-9@users.noreply.github.com>
2022-08-14 14:12:15 +0900

    update pref_daily date column

--- models/pref_daily.sql
+++ models/pref_daily.sql
@@ -2,7 +2,7 @@ SELECT
       pref.prefCode,
       pref.prefJP,
       pref.prefEN,
-      year || month || date AS "date",
+      MAKE_DATE(pref_daily.year, pref_daily.month, pref_daily.date) AS "date",
       pref_daily.peopleTested AS tests,
       pref_daily.testedPositive AS newCases,
       pref_daily.hospitalized AS activeCases,
